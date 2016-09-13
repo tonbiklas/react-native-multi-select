dcb5ef1695755f49721ba55e51ed12ce6b733eb2
Hassan Gilak <hasan.gilak@outlook.com>
2016-09-13 16:21:54 +0430

    requires things from dist now

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.Select = require('./dist/Select').default;
+exports.SelectTextBox = require('./dist/SelectTextBox').default;
+exports.Option = require('./dist/Option').default;
+exports.OptionList = require('./dist/OptionList').default;
