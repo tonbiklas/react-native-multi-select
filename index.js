d0d8d3233633ba124b7acc3625e5a907309935c8
Hassan Gilak <hasan.gilak@outlook.com>
2016-09-13 19:19:16 +0430

    change exports

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import {Option} from './multi-select/Option';
 import {OptionList} from './multi-select/OptionList';
 import {SelectTextBox} from './multi-select/SelectTextBox';
 
-export let Select = Select;
-export let SelectTextBox = SelectTextBox;
-export let Option = Option;
-export let OptionList = OptionList;
+export default {
+    Select, Option, OptionList, SelectTextBox
+};
